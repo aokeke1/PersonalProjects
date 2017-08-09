5dd229cd4e79e398ea2606647fc247eec4ed4642
aokeke1 <arinzeokeke0@gmail.com>
2017-08-09 15:37:32 -0400

    Starting tic tac toe board class

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -5,3 +5,20 @@ Created on Wed Aug  9 15:21:37 2017
 @author: aokeke
 """
 
+import numpy as np
+
+class TicTacTieBoard:
+    def init(self,xPos,oPos):
+        self.board = np.empty((3,3),dtype=str)
+        for p in xPos:
+            self.board[p] = "X"
+        for p in oPos:
+            self.board[p] = "O"
+    
+    def __hash__(self):
+        return hash(self.board.tostring())
+    
+    def getXPos(self):
+        return np.argwhere(self.board=="X")
+    def getOPos(self):
+        return np.argwhere(self.board=="O")
