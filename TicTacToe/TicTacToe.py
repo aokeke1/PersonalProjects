74a7606cb015d247af42204bbf967c090e77afb5
aokeke1 <arinzeokeke0@gmail.com>
2017-08-09 17:10:21 -0400

    Updates to board class

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -7,8 +7,8 @@ Created on Wed Aug  9 15:21:37 2017
 
 import numpy as np
 
-class TicTacTieBoard:
-    def init(self,xPos,oPos):
+class TicTacToeBoard:
+    def __init__(self,xPos = [],oPos = []):
         self.board = np.empty((3,3),dtype=str)
         for p in xPos:
             self.board[p] = "X"
@@ -22,3 +22,20 @@ class TicTacTieBoard:
         return np.argwhere(self.board=="X")
     def getOPos(self):
         return np.argwhere(self.board=="O")
+    def makeMove(self,position,team):
+        if team=="X":
+            xPosOld = self.getXPos()
+            oPos = self.getOPos()
+            xPos = np.zeros((len(xPosOld)+1,2),dtype=int)
+            xPos[0:len(xPosOld),:] = xPosOld
+            xPos[len(xPosOld),:] = position
+        elif team=="O":
+            xPos = self.getXPos()
+            oPosOld = self.getOPos()
+            oPos = np.zeros((len(oPosOld)+1,2),dtype=int)
+            oPos[0:len(oPosOld),:] = xPosOld
+            oPos[len(oPosOld),:] = position
+        newBoard = self.init(xPos,oPos)
+        return newBoard
+    def __str__(self):
+        return " "+str(self.board)[1:-1]
